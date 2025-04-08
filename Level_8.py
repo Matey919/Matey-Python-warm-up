a2aa7109748de9c01ae332b2ba4ce6522debc77e
mac <mac@heimeis-MacBook-Pro.local>
2025-04-08 19:55:17 -0400

    Level 8 complete

---
+++ Level_8.py
@@ -0,0 +1,7 @@
+input ("What is your name?")
+name = input
+#The program will pause and wait for the user to type in the prompt
+
+question = "What's your name?"
+name = input(question)
+#In this way we make question as the variable and "What's your name?" as the value, now the variable is passing through the input instead of a string
