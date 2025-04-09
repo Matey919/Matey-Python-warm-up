e08615aed9b1a8b3ac9ca8613df5fdeadefebb93
mac <mac@heimeis-MacBook-Pro.local>
2025-04-08 20:07:40 -0400

    level 9 problem 2 complete

---
+++ Level_9_Problem_2.py
@@ -0,0 +1,17 @@
+Number_1 = input("What's the first number?")
+Number_2 = input("What's the first number?")
+
+Number_1 = float(Number_1)
+Number_2 = float(Number_2)
+
+if (Number_1 > Number_2):
+    print ("Number1 > Number2")
+
+elif (Number_1 < Number_2):
+    print ("Number1 < Number2")
+
+else:
+    print ("They are equal")
+
+
+
